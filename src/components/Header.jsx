9afc70b5b61b81020426000758494a24ee8ff5e9
rindhiya970 <rindhiya731@gmail.com>
2026-05-09 12:46:00 +0530

    added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,8 @@ function Header() {
     <header className="header">
       <div className="header-emoji">🎵</div>
       <h1>Vibe Player</h1>
-      <p className="header-sub">Turn it up ✨</p>
+      <p className="header-sub">your music, your mood ✨</p>
+      <div className="song-count">20 tracks ready to play</div>
     </header>
   );
 }
