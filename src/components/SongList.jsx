8b0d301f88d42030ad4905f27b9218c3a50696cc
rindhiya970 <rindhiya731@gmail.com>
2026-05-09 13:29:46 +0530

    FEATURE

--- src/components/SongList.jsx
+++ src/components/SongList.jsx
@@ -1,25 +1,29 @@
 import SongCard from "./SongCard";
 import "./SongList.css";
 
-function SongList({ songs, currentSongIndex, setCurrentSongIndex, setIsPlaying, addToPlaylist, playlist }) {
+function SongList({ songs, currentSongIndex, setCurrentSongIndex, setIsPlaying, addToPlaylist, playlist, allSongs }) {
   const handlePlay = (id) => {
-    const index = songs.findIndex((s) => s.id === id);
+    // Find index in the full song list, not filtered list
+    const index = allSongs.findIndex((s) => s.id === id);
     setCurrentSongIndex(index);
     setIsPlaying(true);
   };
 
   return (
     <div className="song-list">
-      {songs.map((song, index) => (
-        <SongCard
-          key={song.id}
-          song={song}
-          isCurrent={index === currentSongIndex}
-          handlePlay={handlePlay}
-          addToPlaylist={addToPlaylist}
-          isInPlaylist={playlist.includes(song.id)}
-        />
-      ))}
+      {songs.map((song) => {
+        const index = allSongs.findIndex((s) => s.id === song.id);
+        return (
+          <SongCard
+            key={song.id}
+            song={song}
+            isCurrent={index === currentSongIndex}
+            handlePlay={handlePlay}
+            addToPlaylist={addToPlaylist}
+            isInPlaylist={playlist.includes(song.id)}
+          />
+        );
+      })}
     </div>
   );
 }
